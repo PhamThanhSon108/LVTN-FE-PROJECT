4f97f17ccc39122929cc0c5e226813c5a2ff5c25
son.pt2001 <son.pt@geekup.vn>
2023-05-01 00:25:44 +0700

    feat:crud categories

--- admin/src/Redux/Actions/CategoryActions.js
+++ admin/src/Redux/Actions/CategoryActions.js
@@ -13,22 +13,39 @@ import {
   CATEGORY_UPDATE_FAIL,
   CATEGORY_UPDATE_REQUEST,
   CATEGORY_UPDATE_SUCCESS,
+  UPDATE_WHEN_ADD_CATEGORY_SUCCESS,
+  UPDATE_WHEN_DELETE_CATEGORY_SUCCESS,
 } from '../Constants/CategoryConstants';
 
-export const ListCategory = () => async (dispatch, getState) => {
+export const ListCategory =
+  (level = 2) =>
+  async (dispatch, getState) => {
+    try {
+      dispatch({ type: CATEGORY_REQUEST });
+      const {
+        userLogin: { userInfo },
+      } = getState();
+
+      const config = {
+        headers: {
+          Authorization: `Bearer ${userInfo.accessToken}`,
+        },
+      };
+      const { data } = await request.get(`/categories`, { params: { level: level } }, config);
+      dispatch({ type: CATEGORY_SUCCESS, payload: data.data?.categories || [] });
+    } catch (error) {
+      dispatch({
+        type: CATEGORY_FAIL,
+        payload: error.response && error.response.data.message ? error.response.data.message : error.message,
+      });
+    }
+  };
+
+export const FetchCategoriesTree = () => async (dispatch, getState) => {
   try {
     dispatch({ type: CATEGORY_REQUEST });
-    const {
-      userLogin: { userInfo },
-    } = getState();
-
-    const config = {
-      headers: {
-        Authorization: `Bearer ${userInfo.accessToken}`,
-      },
-    };
-    const { data } = await request.get(`/categories?level=2`, config);
-    dispatch({ type: CATEGORY_SUCCESS, payload: data.data?.categories || [] });
+    const { data } = await request.get('/categories/get-category-tree');
+    dispatch({ type: CATEGORY_SUCCESS, payload: data?.data?.categories || [] });
   } catch (error) {
     dispatch({
       type: CATEGORY_FAIL,
@@ -37,67 +54,67 @@ export const ListCategory = () => async (dispatch, getState) => {
   }
 };
 
-export const DeleteCategory = (i) => async (dispatch, getState) => {
-  try {
-    dispatch({ type: CATEGORY_DELETE_REQUEST });
-    const {
-      userLogin: { userInfo },
-    } = getState();
+export const DeleteCategory =
+  ({ id, statusDeleteCategory }) =>
+  async (dispatch, getState) => {
+    try {
+      dispatch({ type: CATEGORY_DELETE_REQUEST });
 
-    const config = {
-      headers: {
-        Authorization: `Bearer ${userInfo.accessToken}`,
-      },
-    };
-    const { data } = await request.delete(`/categories/${i}`, config);
-    dispatch({ type: CATEGORY_DELETE_SUCCESS, payload: data });
-  } catch (error) {
-    dispatch({
-      type: CATEGORY_DELETE_FAIL,
-      payload: error.response && error.response.data.message ? error.response.data.message : error.message,
-    });
-  }
-};
+      const { data } = await request.delete(`/categories/${id}`);
+      dispatch({ type: CATEGORY_DELETE_SUCCESS, payload: data });
+      dispatch({ type: UPDATE_WHEN_DELETE_CATEGORY_SUCCESS, payload: id });
+      statusDeleteCategory.success();
+    } catch (error) {
+      const message = error.response && error.response.data.message ? error.response.data.message : error.message;
+      dispatch({
+        type: CATEGORY_DELETE_FAIL,
+        payload: message,
+      });
+      statusDeleteCategory.error(message);
+    } finally {
+      statusDeleteCategory.finally();
+    }
+  };
 
-export const addCategory = (name, description) => async (dispatch, getState) => {
-  try {
-    dispatch({ type: CATEGORY_ADD_REQUEST });
-    const {
-      userLogin: { userInfo },
-    } = getState();
+export const AddCategory =
+  ({ category = { level: 1, name: '', description: '' }, createCategoryStatus }) =>
+  async (dispatch) => {
+    try {
+      dispatch({ type: CATEGORY_ADD_REQUEST });
 
-    const config = {
-      headers: {
-        Authorization: `Bearer ${userInfo.accessToken}`,
-      },
-    };
-    const { data } = await request.post(`/categories`, { name, description }, config);
-    dispatch({ type: CATEGORY_ADD_SUCCESS, payload: data });
-  } catch (error) {
-    dispatch({
-      type: CATEGORY_ADD_FAIL,
-      payload: error.response && error.response.data.message ? error.response.data.message : error.message,
-    });
-  }
-};
-export const UpdateCurrentCategory = (idCategory, name, description) => async (dispatch, getState) => {
-  try {
-    dispatch({ type: CATEGORY_UPDATE_REQUEST });
-    const {
-      userLogin: { userInfo },
-    } = getState();
+      const { data } = await request.post(`/categories`, category);
+      dispatch({ type: CATEGORY_ADD_SUCCESS, payload: data });
+      dispatch({ type: UPDATE_WHEN_ADD_CATEGORY_SUCCESS, payload: data?.data?.newCategory });
 
-    const config = {
-      headers: {
-        Authorization: `Bearer ${userInfo.accessToken}`,
-      },
-    };
-    const { data } = await request.put(`/categories`, { idCategory, name, description }, config);
-    dispatch({ type: CATEGORY_UPDATE_SUCCESS, payload: data });
-  } catch (error) {
-    dispatch({
-      type: CATEGORY_UPDATE_FAIL,
-      payload: error.response && error.response.data.message ? error.response.data.message : error.message,
-    });
-  }
-};
+      createCategoryStatus.success();
+    } catch (error) {
+      const message = error.response && error.response.data.message ? error.response.data.message : error.message;
+      dispatch({
+        type: CATEGORY_ADD_FAIL,
+        payload: message,
+      });
+      createCategoryStatus.error(message);
+    } finally {
+      createCategoryStatus.finally();
+    }
+  };
+export const UpdateCategory =
+  ({ id, category, updateCategoryStatus }) =>
+  async (dispatch, getState) => {
+    try {
+      dispatch({ type: CATEGORY_UPDATE_REQUEST });
+
+      const { data } = await request.put(`/categories/${id}`, category);
+      dispatch({ type: CATEGORY_UPDATE_SUCCESS, payload: data });
+      updateCategoryStatus.success();
+    } catch (error) {
+      const message = error.response && error.response.data.message ? error.response.data.message : error.message;
+      dispatch({
+        type: CATEGORY_UPDATE_FAIL,
+        payload: message,
+      });
+      updateCategoryStatus.error(message);
+    } finally {
+      updateCategoryStatus.finally();
+    }
+  };
--- admin/src/screens/Voucher/Voucher.js
+++ admin/src/screens/Voucher/Voucher.js
@@ -1,276 +1,116 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import styles from './Voucher.module.scss';
-import { Button, Card, FormControlLabel, InputAdornment, Radio, RadioGroup, TextField } from '@mui/material';
-import { MultiInputDateTimeRangeField } from '@mui/x-date-pickers-pro';
+import {
+  Avatar,
+  Button,
+  Card,
+  CardActions,
+  CardHeader,
+  Divider,
+  InputAdornment,
+  List,
+  ListItem,
+  ListItemAvatar,
+  ListItemIcon,
+  ListItemText,
+  MenuItem,
+  Pagination,
+  Select,
+  TextField,
+  Tooltip,
+} from '@mui/material';
 import useVoucher from './hook/useVoucher';
-import { Controller } from 'react-hook-form';
 import { inputPropsConstants } from '../../constants/variants';
-import AddProductToVoucher from './components/AddProductToVoucher/AddProductToVoucher';
 import { Link } from 'react-router-dom';
-import moment from 'moment';
-
-const applyVoucherFor = {
-  allProducts: '1',
-  selectedProducts: '2',
-};
-
-const voucherType = {
-  price: 'price',
-  percent: 'percent',
-};
-
-const DEFAULT_STEP_USABLE_VOUCHER = 7;
-
-const renderError = (errors) => {
-  const message = errors.find((error) => error?.error)?.message;
-  if (message) return <span className={styles.errorMessage}>{message}</span>;
-};
 export default function Voucher() {
   const { control, watch, handleSubmit, handleCreateVoucher } = useVoucher();
   return (
     <div className={styles.voucherContainer}>
-      <Card className={styles.voucherWrapper}>
-        <div className={styles.header}>
-          <Link to="/products" className={styles.backBtn}>
-            <i className="fas fa-arrow-left" />
-          </Link>
-          <h2 className={styles.title}>Tạo Mã giảm giá</h2>
-        </div>
-        <form className={styles.voucherForm} onSubmit={handleSubmit(handleCreateVoucher)}>
-          <Card sx={{ boxShadow: 3 }} className={styles.voucherPropertyArea}>
-            <h5 className={styles?.subTitle}>Thông tin cơ bản</h5>
-            <Controller
-              name="name"
-              control={control}
-              rules={{ required: true }}
-              render={({ field, fieldState }) => (
-                <TextField
-                  focused={!!fieldState.error}
-                  color={fieldState.error ? 'error' : 'info'}
-                  className={styles.voucherFormItem}
-                  label="Tên chương trình giảm giá"
-                  {...field}
-                  variant={inputPropsConstants.variantOutLine}
-                  size={inputPropsConstants.smallSize}
-                  helperText={renderError([
-                    { error: fieldState?.error?.type === 'required', message: 'Bạn chưa nhập trường này' },
-                  ])}
-                />
-              )}
-            />
-            <Controller
-              name="code"
-              control={control}
-              rules={{ required: true }}
-              render={({ field, fieldState }) => (
-                <TextField
-                  focused={!!fieldState.error}
-                  color={fieldState.error ? 'error' : 'info'}
-                  className={styles.voucherFormItem}
-                  label="Mã voucher"
-                  {...field}
-                  variant={inputPropsConstants.variantOutLine}
-                  size={inputPropsConstants.smallSize}
-                  helperText={renderError([
-                    { error: fieldState?.error?.type === 'required', message: 'Bạn chưa nhập trường này' },
-                  ])}
-                />
-              )}
-            />
-
-            <Controller
-              name="applyTime"
-              control={control}
-              render={({ field }) => (
-                <MultiInputDateTimeRangeField
-                  {...field}
-                  size={inputPropsConstants.smallSize}
-                  defaultValue={[moment(), moment().add(DEFAULT_STEP_USABLE_VOUCHER, 'days')]}
-                  slotProps={{
-                    textField: ({ position }) => ({
-                      className: styles.voucherFormItem,
-                      size: inputPropsConstants.smallSize,
-                      label: position === 'start' ? 'Có hiệu lực từ' : 'Ngày hết hạn',
-                    }),
-                  }}
-                />
-              )}
-            />
-
-            <Controller
-              name="applyFor"
-              defaultValue={applyVoucherFor.selectedProducts}
-              control={control}
-              render={({ field }) => (
-                <RadioGroup {...field} row className={styles.applyForProductWrapper}>
-                  <FormControlLabel
-                    value={applyVoucherFor.allProducts}
-                    control={<Radio size={inputPropsConstants.smallSize} />}
-                    label="Áp dụng cho tất cả sản phẩm"
-                  />
-                  <FormControlLabel
-                    value={applyVoucherFor.selectedProducts}
-                    control={<Radio size={inputPropsConstants.smallSize} />}
-                    label="Chọn sản phẩm"
-                  />
-                </RadioGroup>
-              )}
-            />
-
-            {watch('applyFor') === applyVoucherFor.selectedProducts ? <AddProductToVoucher /> : null}
-          </Card>
-          <Card sx={{ boxShadow: 3 }} bo className={styles.voucherPropertyArea}>
-            <h5 className={styles?.subTitle}>Thiết lập khuyến mãi</h5>
-            <Controller
-              name="discountType"
-              control={control}
-              render={({ field }) => (
-                <RadioGroup
-                  label="Loại giảm giá"
-                  placeholder="Chọn thể loại"
-                  {...field}
-                  className={styles.applyForProductWrapper}
-                  defaultValue={voucherType.price}
-                  row
-                >
-                  <FormControlLabel
-                    value={voucherType.price}
-                    control={<Radio size={inputPropsConstants.smallSize} />}
-                    label="Giá cố định"
-                  />
-                  <FormControlLabel
-                    value={voucherType.percent}
-                    control={<Radio size={inputPropsConstants.smallSize} />}
-                    label="Theo %"
-                  />
-                </RadioGroup>
-              )}
-            />
-            {watch('discountType') === voucherType.price ? (
-              <Fragment>
-                <Controller
-                  name="discount"
-                  control={control}
-                  rules={{ required: true, max: 1000000000000, min: 0.01 }}
-                  render={({ field, fieldState }) => (
-                    <TextField
-                      color={fieldState.error ? 'warning' : 'info'}
-                      size={inputPropsConstants.smallSize}
-                      className={styles.voucherFormItem}
-                      label="Giảm giá"
-                      InputProps={{
-                        type: 'number',
-                        endAdornment: <InputAdornment position="end">VNĐ</InputAdornment>,
-                      }}
-                      {...field}
-                      variant={inputPropsConstants.variantOutLine}
-                      helperText={renderError([
-                        { error: fieldState?.error?.type === 'required', message: 'Bạn chưa nhập trường này' },
-                        {
-                          error: fieldState?.error?.type === 'min',
-                          message: 'Giảm giá phải có giá trị lớn hơn 0',
-                        },
-                        {
-                          error: fieldState?.error?.type === 'max',
-                          message: 'Giá trị giảm giá quá lớn',
-                        },
-                      ])}
-                    />
-                  )}
-                />
-              </Fragment>
-            ) : (
-              <Controller
-                name="discount"
-                rules={{ required: true, max: 100, min: 0 }}
-                control={control}
-                render={({ field, fieldState }) => (
-                  <TextField
-                    focused={!!fieldState.error}
-                    color={fieldState.error ? 'error' : 'info'}
-                    size={inputPropsConstants.smallSize}
-                    className={styles.voucherFormItem}
-                    label="Giảm giá"
-                    InputProps={{
-                      endAdornment: <InputAdornment position="end">%</InputAdornment>,
-                      type: 'number',
-                    }}
-                    {...field}
-                    variant={inputPropsConstants.variantOutLine}
-                    helperText={renderError([
-                      { error: fieldState?.error?.type === 'required', message: 'Bạn chưa nhập trường này' },
-                      {
-                        error: fieldState?.error?.type === 'max' || fieldState?.error?.type === 'min',
-                        message: 'Phần trăm phải có giá trị từ 0 tới 100',
-                      },
-                    ])}
-                  />
-                )}
-              />
-            )}
-
-            <Controller
-              name="usageLimit"
-              control={control}
-              rules={{ required: true, max: 100, min: 0, pattern: /^[1-9]\d*$/ }}
-              render={({ field, fieldState }) => (
-                <TextField
-                  focused={!!fieldState.error}
-                  color={fieldState.error ? 'error' : 'info'}
-                  size={inputPropsConstants.smallSize}
-                  className={styles.voucherFormItem}
-                  label="Tổng số lượt sử dụng tối đa"
-                  {...field}
-                  variant={inputPropsConstants.variantOutLine}
-                  InputProps={{
-                    endAdornment: <InputAdornment position="end">Lượt</InputAdornment>,
-                    type: 'number',
-                  }}
-                  helperText={renderError([
-                    { error: fieldState?.error?.type === 'required', message: 'Bạn chưa nhập trường này' },
-                    {
-                      error: fieldState?.error?.type === 'min' || fieldState?.error?.type === 'pattern',
-                      message: 'Số lượt giảm giá phải là số nguyên lớn hơn 0',
-                    },
-                  ])}
-                />
-              )}
-            />
-
-            <Controller
-              name="usageLimitPer"
-              control={control}
-              render={({ field }) => (
-                <TextField
-                  size={inputPropsConstants.smallSize}
-                  className={styles.voucherFormItem}
-                  label="Lượt sử dụng tối đa/ người mua"
-                  value="1"
-                  variant={inputPropsConstants.variantOutLine}
-                  disabled
-                />
-              )}
-            />
-          </Card>
-          <Card className={styles.voucherAction} sx={{ boxShadow: 3 }}>
-            <Link to="/vouchers">
-              <Button
-                className={styles.cancelBtn}
+      <div className={styles.header}>
+        <h2 className={styles.title}>Danh sách mã giảm giá</h2>
+        <Link to="/vouchers/add">
+          <Button
+            type="submit"
+            variant={inputPropsConstants.variantContained}
+            startIcon={<i className="fas fa-plus-circle" />}
+          >
+            Tạo mã
+          </Button>
+        </Link>
+      </div>
+      <Card className={styles.voucherListWrapper}>
+        <CardHeader
+          className={styles.vouchersHeader}
+          avatar={
+            <div className={styles.vouchersHeaderFilter}>
+              <TextField
+                className={styles.searchVouchers}
+                label="Tìm kiếm sản phẩm"
+                size={inputPropsConstants.smallSize}
                 variant={inputPropsConstants.variantOutLine}
-                startIcon={<i className="fas fa-mark" />}
+                InputProps={{
+                  endAdornment: (
+                    <InputAdornment position="end">
+                      <i className="fas fa-search" />
+                    </InputAdornment>
+                  ),
+                }}
+              />
+              <Select
+                className={styles.searchVouchers}
+                size={inputPropsConstants.smallSize}
+                placeholder="Chọn thể loại"
               >
-                Hủy
-              </Button>
-            </Link>
-            <Button
-              type="submit"
-              variant={inputPropsConstants.variantContained}
-              startIcon={<i className="fas fa-plus-circle" />}
-            >
-              Tạo mã
-            </Button>
-          </Card>
-        </form>
+                <MenuItem value="-1">Tất cả thể loại</MenuItem>
+                {/* {categories.map((category) => (
+                  <MenuItem value={category?._id}>{category?.name || ''}</MenuItem>
+                ))} */}
+              </Select>
+            </div>
+          }
+        />
+        <Divider />
+
+        <List
+          className={styles.voucherList}
+          sx={{
+            bgcolor: 'background.paper',
+            overflow: 'auto',
+          }}
+          dense
+          component="div"
+          role="list"
+        >
+          {[1, 2, 3, 4].map((value) => {
+            const labelId = `transfer-list-all-item-${value}-label`;
+
+            return (
+              <ListItem key={value} role="listitem" button>
+                <ListItemAvatar>
+                  <Avatar alt={value?.name} src={value?.images?.[0]} />
+                </ListItemAvatar>
+                <ListItemText id={labelId} primary={value?.name || 'Tên sản phẩm'} />
+                <ListItemText id={labelId} primary={value?.category?.name || 'Thể loại sản phẩm'} />
+                <ListItemText id={labelId} primary={value?.price + ' VNĐ' || 'Giá sản phẩm'} />
+                <ListItemIcon>
+                  <div className={`${styles.actionEachVoucher} ${styles.actionEditVoucher}`}>
+                    <Tooltip title="Chỉnh sửa mã giảm giá">
+                      <i className="fas fa-pencil color-red" />
+                    </Tooltip>
+                  </div>
+                  <div className={`${styles.actionEachVoucher} ${styles.actionDeleteVoucher}`}>
+                    <Tooltip title="Xóa mã giảm giá">
+                      <i className="fas fa-trash-alt color-red" />
+                    </Tooltip>
+                  </div>
+                </ListItemIcon>
+              </ListItem>
+            );
+          })}
+        </List>
+        <CardActions className={styles.pagination}>
+          <Pagination variant="outlined" shape="rounded" />
+        </CardActions>
       </Card>
     </div>
   );
