3309c8fd5cadee318202fb3dbee4590016470dd6
son.pt2001 <son.pt@geekup.vn>
2023-05-23 02:15:06 +0700

    feat: change url request

--- user/src/utils/request.js
+++ user/src/utils/request.js
@@ -5,7 +5,7 @@ const getToken = () => {
     return `Bearer ${JSON.parse(localStorage.getItem('userInfo'))?.accessToken}`;
 };
 
-export const API_FASHIONSHOP = 'https://3824-14-169-194-38.ngrok-free.app/api/v1/';
+export const API_FASHIONSHOP = 'https://api.fashionshop.nkt2001.tech/api/v1/';
 
 const request = axios.create({
     baseURL: API_FASHIONSHOP,
